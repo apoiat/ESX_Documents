a05f58a7e566fe4a44bbd7411a4d9f76066b374c
Ap. Iatridis <apo.iatridis@gmail.com>
2019-05-29 22:09:22 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,6 @@ Config.MenuKey 			= 182 -- "L" button, change to whatever you want
 Config.UseCustomFonts 	= true -- Leave this as is if you don't know how to or haven't loaded custom fonts.
 Config.CustomFontFile 	= "greek" -- change only if you turn custom fonts on.
 Config.CustomFontId 	= "OpenSans" -- change only if you turn custom fonts ok.
-Config.Locale 			= "gr" -- gr / en
+Config.Locale 			= "en" -- gr / en
 
 Config.Documents = {}
