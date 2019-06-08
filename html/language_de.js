681cc9599807f3b4f3a3aea27ff2c5bb29b2aaaa
Ap. Iatridis <apo.iatridis@gmail.com>
2019-06-08 10:37:01 +0300

    DE translation

---
+++ html/language_de.js
@@ -0,0 +1,17 @@
+var _U = {
+
+	"sign_terms" : [
+		"Dieses Dokument wird nach seiner Unterzeichnung zu einem Rechtlich bindenden Dokument.",
+		"Durch die Unterzeichnung akzeptieren sie alle rechtlichen Konsequenzen, die sich daraus ergeben.",
+		"Jede Kopie ist dem Original gleichwertig. Seien Sie besonders vorsichtig, wenn Sie Kopien geben.",
+		"Stellen Sie sicher, dass Sie den Inhalt dieses Dokuments vollständig kennen, bevor Sie unterschreiben.",
+		"Zögern Sie nicht, sich vor der Unterzeichnung an einen Rechtsbeistand zu wenden."
+	],
+	"title_information" : "INFORMATION",
+	"title_fill_data" 	: "FÜLLEN SIE DIE FOLGENDEN FELDER AUS",
+	"terms_and_signing" : "BEDINGUNGEN & UNTERSCHRIFT",
+	"first_name" 		: "Vorname:",
+	"last_name" 		: "Nachname:",
+	"birth_date" 		: "Geburtsdatum:",
+	"occupation" 		: "Besetzung:"
+}
