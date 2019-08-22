1ce79d1aabe3b67e4fa5f65aa58520ea2a865f69
KandaSoranyan <32266067+KandaSoranyan@users.noreply.github.com>
2019-08-22 13:42:05 +0200

    adding fr translation

---
+++ html/language_fr.js
@@ -0,0 +1,17 @@
+var _U = {
+
+	"sign_terms" : [
+		"Lorsque signé, ce document devient officiel et légal",
+		"En signant ce document, vous êtes légalement liés à son contexte et acceptez les conséquences que ce document peut entrainé.",
+		"Chaque duplicata de ce document a une valeur identique à l'original. Soyez prudent lorsque vous en donnez une copie.",
+		"Soyez sûr d'être conscient et avoir pris connaissance de ce document avant de le signer.",
+		"N'hésitez pas à faire appel à un notaire légal avant la signature du document.""
+	],
+	"title_information" : "INFORMATIONS",
+	"title_fill_data" 	: "REMPLISSEZ LES CHAMPS SUIVANTS",
+	"terms_and_signing" : "CONDITIONS ET SIGNATURE",
+	"first_name" 		: "Prénom:",
+	"last_name" 		: "Nom:",
+	"birth_date" 		: "Date de naissance:",
+	"occupation" 		: "Emploi:"
+}
